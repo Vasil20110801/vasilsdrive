16043c03797fa5785297430d8c7be640dc670229
Zen2re <vasildyulgerski04@gmail.com>
2025-06-13 12:01:08 +0300

    Първи комит: vasilsdrive

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory, session
+import os
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'
+
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    if 'username' not in session:
+        return redirect(url_for('login'))
+    files = os.listdir(UPLOAD_FOLDER)
+    return render_template('index.html', files=files, username=session['username'])
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'admin' and password == 'password':
+            session['username'] = username
+            return redirect(url_for('index'))
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'username' not in session:
+        return redirect(url_for('login'))
+    if 'file' not in request.files:
+        return redirect(url_for('index'))
+    f = request.files['file']
+    if f.filename != '':
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
+    return redirect(url_for('index'))
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
