dac2f37a50d5b08d61fea8cda62c422342857220
Zen2re <vasildyulgerski04@gmail.com>
2025-06-13 14:24:10 +0300

    ee

--- app.py
+++ app.py
@@ -123,8 +123,10 @@ def delete(filename):
 with app.app_context():
     db.create_all()
 
+
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
-app.run(debug=True)
+        import os
+    app.run(host='0.0.0.0', port=port)
